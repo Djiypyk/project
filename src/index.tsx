8fdc2265bf7d65786858d3ae8322734644fd6544
Mikalai Zarazaka <golmtz@gmail.com>
2023-06-14 18:36:19 +0300

    fix: add FSD architecture

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@
 // import {render} from 'react-dom'
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import ThemeProvider from './theme/ThemeProvider';
+
+import App from './app/App';
+import { ThemeProvider } from './app/providers/ThemeProvider';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
@@ -12,7 +14,6 @@ root.render(
         <ThemeProvider>
             <App />
         </ThemeProvider>
-      
     </BrowserRouter>
 );
 
