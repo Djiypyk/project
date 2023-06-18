39ba98ba21386051146a12283b0ba5772636077d
Zarazaka Mikalai <golmtz@gmail.com>
2023-06-18 17:21:51 +0300

    fix: webpack plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -30,21 +30,5 @@ export function buildPlugins({
         )
     }
 
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
-    ]
+    return plugins
 }
