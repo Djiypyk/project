9be875b91632b63c2f2413dba7ebbf00ec4749ce
Zarazaka Mikalai <golmtz@gmail.com>
2023-06-18 22:21:41 +0300

    feat: upgrade sidebar, add theme button

--- src/shared/lib/tests/componentsRender/componentsRender.tsx
+++ src/shared/lib/tests/componentsRender/componentsRender.tsx
@@ -2,19 +2,19 @@ import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router';
 
-export interface renderWithRouterOptions {
-    route: string
+export interface ComponentsRenderOptions {
+    route?: string
 }
 
-export function renderWithTranslation(
+export function componentsRender(
     component: ReactNode,
-    options: renderWithRouterOptions,
+    options: ComponentsRenderOptions = {},
 ) {
-    const {route} = options
+    const { route = '/' } = options;
     return render(
-        <MemoryRouter initialEntries={[]}>
+        <MemoryRouter initialEntries={[route]}>
             <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
         </MemoryRouter>,
     );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -3,15 +3,16 @@ import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 import {
     renderWithTranslation,
 } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentsRender } from 'shared/lib/tests/componentsRender/componentsRender';
 
 describe('Sidebar', () => {
     test('with only first param', () => {
-        renderWithTranslation(<Sidebar />);
+        componentsRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentsRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
