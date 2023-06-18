c3122d3e864ce5ef4023e8899834a49ca7c30829
Zarazaka Mikalai <golmtz@gmail.com>
2023-06-18 17:29:58 +0300

    fix: linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,65 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'prettier', 'plugin:storybook/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['react', 'i18next'],
-  rules: {
-    'react/jsx-indent': [2, 4],
-    'react/jsx-indent-props': [2, 4],
-    indent: [2, 4],
-    'react/jsx-filename-extension': [2, {
-      extensions: ['.js', '.jsx', '.ss', '.tsx']
-    }],
-    'react/react-in-jsx-scope': 'off',
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'react/require-default-props': 'off',
-    'react/function-component-definition': 'off',
-    'no-var-requires': 0,
-    'react/button-has-type': 'off',
-    'import/extensions': 'off',
-    'no-unused-vars': 'warn',
-    'no-use-before-define': 'off',
-    'no-shadow': 'off',
-    'react/jsx-props-no-spreading': 'warn',
-    'import/no-extraneous-dependencies': 'warn',
-    'no-underscore-dangle': 'warn',
-    'i18next/no-literal-string': ['warn', {
-      markupOnly: true,
-      ignoreAttribute: ['data-testid']
-    }],
-    'max-len': ['error', {
-      ignoreComments: true,
-      code: 100
-    }]
-  },
-  globals: {
-    __IS_DEV__: true
-  },
-  overrides: [{
-    files: ['**/src/**/*.test.{ts,tsx}'],
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+        // 'prettier',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'i18next'],
     rules: {
-      'i18next/no-literal-string': 'off'
-    }
-  }]
-};
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.ss', '.tsx'] },
+        ],
+        'react/react-in-jsx-scope': 'off',
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'react/require-default-props': 'off',
+        'react/function-component-definition': 'off',
+        'no-var-requires': 0,
+        'react/button-has-type': 'off',
+        'import/extensions': 'off',
+        'no-unused-vars': 'warn',
+        'no-use-before-define': 'off',
+        'no-shadow': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'import/no-extraneous-dependencies': 'warn',
+        'no-underscore-dangle': 'warn',
+        'i18next/no-literal-string': ['warn', { markupOnly: true }],
+        'max-len': [
+            'error',
+            {
+                ignoreComments: true,
+                code: 100,
+            },
+        ],
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
+}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
-    className?: string;
+    className?: string
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
@@ -19,12 +19,11 @@ export const Sidebar = ({ className }: SidebarProps) => {
     return (
         <div
             data-testid="sidebar"
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+                className,
+            ])}
         >
-            <Button
-                data-testid="sidebar-toggle"
-                onClick={onToggle}
-            >
+            <Button data-testid="sidebar-toggle" onClick={onToggle}>
                 toggle
             </Button>
             <div className={cls.switchers}>
